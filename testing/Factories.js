55c070d3a703e4f700eebd20cfa9a4b87f9fa3c4
Andres Flores <andresflo883@gmail.com>
2023-06-30 15:52:36 -0400

    added checkWin and receiveAttack functions)

--- testing/Factories.js
+++ testing/Factories.js
@@ -32,27 +32,56 @@ function GameBoard() {
   const shipLengths = {
     'carrier': 5,
     'battleship': 4,
-    'cruiser': 3,
+    'destroyer': 3,
     'submarine': 3,
-    'destroyer': 2
+    'patrolBoat': 2
+  }
+  const ships = new Set(['carrier', 'battleship', 'destroyer', 'submarine', 'patrolBoat'])
+
+  function receiveAttack([x, y]) {
+    const ship = board[x][y]
+    if (ships.has(ship)) {
+      board[x][y] = 'x'
+      return true
+    } else {
+      board[x][y] = ''
+      return false
+    }
   }
 
-  // function addShip(shipName, length) {
-  //   const ship = Ship(shipName, length)
-  //   ships.push(ship)
-  // }
+  function checkWin() {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const ship = board[i][j]
+        if (ships.has(ship)) {
+          return false
+        }
+      }
+    }
+    return true
+  }
 
-  function placeShip(startCoord, axis, shipName) {
-    const startX = startCoord[0]
-    const startY = startCoord[1]
-
+  function placeShip([x, y], axis, shipName) {
     if (axis === 'x') {
-      for(let i = startY; i < startY + shipLengths[shipName]; i++) {
-        board[startX][i] = shipName
+      if (y + shipLengths[shipName] > 9) return
+      // scan from startCoord along x, make sure spots are empty
+      for (let i = y; i < y + shipLengths[shipName]; i++) {
+        if (board[x][i] !== "") return
+      }
+
+      for (let i = y; i < y + shipLengths[shipName]; i++) {
+        board[x][i] = shipName
       }
     } else if (axis === 'y') {
-      for(let i = startX; i < startX + shipLengths[shipName]; i++) {
-        board[i][startY] = shipName
+      if (x + shipLengths[shipName] > 9) return
+
+      // scan from startCoord along y, make sure spots are empty
+      for (let i = x; i < x + shipLengths[shipName]; i++) {
+        if (board[i][y] !== "") return
+      }
+
+      for (let i = x; i < x + shipLengths[shipName]; i++) {
+        board[i][y] = shipName
       }
     }
   }
@@ -62,11 +91,7 @@ function GameBoard() {
   }
 
 
-  // function getShips() {
-  //   return ships
-  // }
-
-  return { getBoard, placeShip }
+  return { getBoard, placeShip, receiveAttack, checkWin }
 }
 
 module.exports = { Ship, GameBoard }
--- testing/gameboard.test.js
+++ testing/gameboard.test.js
@@ -1,6 +1,8 @@
 const { GameBoard } = require('./Factories.js')
-const { emptyBoard, boardWithDestroyerAlongX, boardWithSubmarineAlongX, boardWithBattleShipAlongX } = require('./boards.js')
+const { emptyBoard, boardWithPatrolBoatAlongX, boardWithPatrolBoatAlongY, boardWithSubmarineAlongX, boardWithBattleShipAlongX, boardWithDestroyerAlongX, boardWithCarrierAlongX, boardWithFiveShipsAlongX, boardWithFiveShipsAlongY, boardWithBattleShipAlongXWithOneHit } = require('./boards.js')
 
+// placeShip() tests
+// startCoord, axis, shipName
 test('init empty board', () => {
   const expected = emptyBoard
   const board = GameBoard()
@@ -8,12 +10,19 @@ test('init empty board', () => {
   expect(result).toEqual(expected)
 })
 
-// startCoord, axis, shipName
-test('place destroyer along x axis', () => {
+test('place patrol boat along x axis', () => {
   const board = GameBoard()
-  board.placeShip([0, 0], 'x', 'destroyer')
+  board.placeShip([0, 0], 'x', 'patrolBoat')
   const result = board.getBoard()
-  const expected = boardWithDestroyerAlongX
+  const expected = boardWithPatrolBoatAlongX
+  expect(result).toEqual(expected)
+})
+
+test('place patrol boat along y axis', () => {
+  const board = GameBoard()
+  board.placeShip([0, 0], 'y', 'patrolBoat')
+  const result = board.getBoard()
+  const expected = boardWithPatrolBoatAlongY
   expect(result).toEqual(expected)
 })
 
@@ -25,11 +34,107 @@ test('place submarine along x axis', () => {
   expect(result).toEqual(expected)
 })
 
-test('place battleship along x axis', () => { 
+test('place destroyer along x axis', () => {
   const board = GameBoard()
+  board.placeShip([0, 0], 'x', 'destroyer')
+  const result = board.getBoard()
+  const expected = boardWithDestroyerAlongX
+  expect(result).toEqual(expected)
+})
+
+test('place battleship along x axis', () => {
+  const board = GameBoard()
+  board.placeShip([0, 0], 'x', 'battleship')
   const result = board.getBoard()
   const expected = boardWithBattleShipAlongX
-
   expect(result).toEqual(expected)
 
+
+})
+
+test('place carrier along x axis', () => {
+  const board = GameBoard()
+  board.placeShip([0, 0], 'x', 'carrier')
+  const result = board.getBoard()
+  const expected = boardWithCarrierAlongX
+  expect(result).toEqual(expected)
+
+})
+
+test('place all five ships along x', () => {
+  const board = GameBoard()
+  board.placeShip([0, 0], 'x', 'carrier')
+  board.placeShip([1, 0], 'x', 'battleship')
+  board.placeShip([2, 0], 'x', 'destroyer')
+  board.placeShip([3, 0], 'x', 'submarine')
+  board.placeShip([4, 0], 'x', 'patrolBoat')
+  const result = board.getBoard()
+  const expected = boardWithFiveShipsAlongX
+  expect(result).toEqual(expected)
+
+})
+
+test('place all five ships along y', () => {
+  const board = GameBoard()
+  board.placeShip([0, 0], 'y', 'carrier')
+  board.placeShip([0, 1], 'y', 'battleship')
+  board.placeShip([0, 2], 'y', 'destroyer')
+  board.placeShip([0, 3], 'y', 'submarine')
+  board.placeShip([0, 4], 'y', 'patrolBoat')
+  const result = board.getBoard()
+  const expected = boardWithFiveShipsAlongY
+  expect(result).toEqual(expected)
+})
+
+test('place 2 ships on top of each other along x', () => {
+  const board = GameBoard()
+  board.placeShip([0, 0], 'x', 'patrolBoat')
+  board.placeShip([0, 1], 'x', 'patrolBoat')
+  const result = board.getBoard()
+  const expected = boardWithPatrolBoatAlongX
+  expect(result).toEqual(expected)
+})
+
+test('place 2 ships on top of each other along y', () => {
+  const board = GameBoard()
+  board.placeShip([0, 0], 'y', 'patrolBoat')
+  board.placeShip([1, 0], 'y', 'patrolBoat')
+  const result = board.getBoard()
+  const expected = boardWithPatrolBoatAlongY
+  expect(result).toEqual(expected)
+})
+
+test('place ship out of bounds along x', () => {
+  const board = GameBoard()
+  board.placeShip([0, 9], 'x', 'carrier')
+  const result = board.getBoard()
+  const expected = emptyBoard
+  expect(result).toEqual(expected)
+})
+
+
+test('place ship out of bounds along y', () => {
+  const board = GameBoard()
+  board.placeShip([9, 0], 'y', 'carrier')
+  const result = board.getBoard()
+  const expected = emptyBoard
+  expect(result).toEqual(expected)
+})
+
+test('hit battleship once', () => { 
+  const board = GameBoard()
+  board.placeShip([0, 0], 'x', 'battleship')
+  board.receiveAttack([0,0])
+  const result = board.getBoard()
+  const expected = boardWithBattleShipAlongXWithOneHit
+  expect(result).toEqual(expected)
+ })
+
+ test('miss the battleship', () => { 
+  const board = GameBoard()
+  board.placeShip([0, 0], 'x', 'battleship')
+  board.receiveAttack([3,3])
+  const result = board.getBoard()
+  const expected = boardWithBattleShipAlongX
+  expect(result).toEqual(expected)
  })
